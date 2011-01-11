cf0161ebf5891eba7d3f36b5afdd4bf52da16135
Mark Birbeck <mark.birbeck@gmail.com>
2011-01-11 18:37:20 +0000

    Added Alex's new layouts.

--- layout-profile.tpl.php
+++ layout-profile.tpl.php
@@ -1,10 +1,11 @@
 <?php include 'page.header.inc'; ?>
 
+
 <div id='left'><div class='navbar clear-block'>
   <?php if (!empty($context_links)): ?>
     <div class='context-links clear-block'><?php print $context_links ?></div>
   <?php endif; ?>
-  <?php if (!empty($left)) print $left ?>
+  <?php print $left ?>
 </div></div>
 
 <div id='canvas' class='clear-block'>
@@ -39,6 +40,18 @@
     	</div>
     <?php endif; ?>
   </div>
+
+  <?php
+    // AS: $left is always shown...
+  	// if ($right): 
+  ?>
+    <div id='right'><div class='sidebar clear-block'>
+      <?php print $mission_block ?>
+      <?php print $right ?>
+    </div></div>
+  <?php 
+    // endif; 
+  ?>
 
 </div>
 
