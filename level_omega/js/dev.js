b9f072fce873a814c5cad12614bf1e3b7181404e
Sungsit Sawaiwan <gibbozer@gmail.com>
2011-06-11 21:21:03 +0700

    Recreate suitable markup for Terminology lists on Accounts Expalained page.

---
+++ level_omega/js/dev.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  /*
+  $('.node dl.terminology').hide();
+  $('.node dl.terminology').after('<div id="termPopup"><h2>POP!</h2></div>');
+  
+  
+  $('.term-list li a').each(function(){
+    // get term texts
+    term = $(this).text();
+    // remove spaces from texts
+    termClass = term.replace(/ /g, '');
+    // assign term classes
+    $(this).addClass(termClass);
+  });
+
+  $('.term-list li a').bind('click', function() {
+    $('.node dl.terminology').slideToggle();
+    return false;
+  });
+*/
+});
