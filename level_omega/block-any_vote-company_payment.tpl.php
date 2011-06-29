2399c38ef3fa46ebb89e6f6a3bc44526809d112b
Sungsit Sawaiwan <gibbozer@gmail.com>
2011-06-30 04:04:01 +0700

    Get all Google chart API & elaborate spacing.

--- level_omega/block-any_vote-company_payment.tpl.php
+++ level_omega/block-any_vote-company_payment.tpl.php
@@ -11,15 +11,23 @@
 
   <div class="score clearfix">
     <img src="https://chart.googleapis.com/chart?cht=pc&chs=115x115&chf=bg,s,f7f7f700&chd=t:-1|<?php print $block->yes_count; ?>,<?php print $block->no_count; ?>&chco=58C327,58C327|ee3322" />
+    
     <div class="widget"><?php print $block->widget ?></div>
-    <div class="percentage"><?php print round($block->yes_count * 100 / $block->vote_sum); ?>%</div>
+    
+    <div class="percentage">
+      <?php if($block->vote_sum == '0'): ?><span>--%</span><?php endif; ?>
+      
+      <?php if($block->vote_sum >> '0'): ?>
+        <span><?php print round($block->yes_count * 100 / $block->vote_sum,1); ?>%</span>
+      <?php endif; ?>
+    </div>
+    
   </div>
 
-  
   <div class="content"><?php print $block->content ?></div>
   
+  <!--
   <div style="font-size:10px;color:red;margin-top:10px;">Debugging: Yes = <?php print $block->yes_count ?> No = <?php print $block->no_count ?> Sum = <?php print $block->vote_sum ?></div>
+  -->
 
-
-  
 </div>
