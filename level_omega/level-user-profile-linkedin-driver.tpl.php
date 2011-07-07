3a2b0487a0c3b2056cb6403115fe6669d5b88027
John Griffin <jg2950@gmail.com>
2011-07-07 11:44:50 +0100

    Re #216 - Add placeholder template for linkedin driver block that's placed on My Account page

---
+++ level_omega/level-user-profile-linkedin-driver.tpl.php
@@ -0,0 +1,9 @@
+<p> level-user-profile-linkedin-driver.tpl.php starts </p>
+
+<p> Are you a company director etc. </p>
+
+<?php
+ print l('Associate your LinkedIn account with your LevelBusiness account', 'user/' . $user->uid . '/edit/linkedin');
+?>
+
+<p> level-user-profile-linkedin-driver.tpl.php ends </p>
