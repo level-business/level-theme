947811e5f7f78580586c7329d5a7ec9ede646a34
Sungsit Sawaiwan <gibbozer@gmail.com>
2011-07-07 19:30:44 +0700

    Theme 216_04_Link_Accounts

--- level_omega/level-user-profile-linkedin-driver.tpl.php
+++ level_omega/level-user-profile-linkedin-driver.tpl.php
@@ -1,9 +1,5 @@
-<p> level-user-profile-linkedin-driver.tpl.php starts </p>
+<p class="intro-text">Are you a company director? Do you want to manage how others see your information? Connect your LinkedIn account in order to claim your profile and to make logging in and out easier.</p>
 
-<p> Are you a company director etc. </p>
-
-<?php
- print l('Associate your LinkedIn account with your LevelBusiness account', 'user/' . $user->uid . '/edit/linkedin');
-?>
-
-<p> level-user-profile-linkedin-driver.tpl.php ends </p>
+<div class="link-button">
+  <a class="linkedin-button" title="Associate your LinkedIn account with your LevelBusiness account" href="/user/<?php print $user->uid ?>/edit/linkedin">Link your account</a>
+</div>
