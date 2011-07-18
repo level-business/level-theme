02c8f127e8065afec2bc773f71475a845773e41c
John Griffin <jg2950@gmail.com>
2011-07-18 17:29:07 +0100

    [#244] Star rating for QOS needs to show both current and user's votes.
    * Fix star width to prevent flickering effect.
    * Move any_vote JS out into a separate file.
    * Scope internal JS var for each any_vote_points_widget.
    * Fix up JS for star widget to reset stars to average on mouseout.
    * Change .average-active back to orange & .highlight to red color.
    * Add star highlight effect.
    * Remove JS behavior and use new any_vote class (average-active) instead.

---
+++ level_omega/js/level_any_vote.js
@@ -0,0 +1,38 @@
+// Star Rating
+$(document).ready(function() {
+
+  // scope internal var for each any_vote_points_widget
+  $('ul.any_vote_points_widget').each(function(){
+
+   // remove other classes to get numeric class names only
+   $(this).children().removeClass('first last');
+
+    // count stars with average-active class
+    var average_count = $('a.average-active').length;
+
+    // highlight stars on rollover only when current user does has not voted yet
+    $('.block-any_vote:not(.user_voted) ul.any_vote_points_widget li').hover(
+
+      // mouseover
+      function(){
+        $('ul.any_vote_points_widget a.average-active').removeClass('average-active');
+        var index = parseInt($(this).attr('class'));
+        // highlight stars below this one
+        for(i=0; i<=index; i++) {
+          $('ul.any_vote_points_widget li').eq(i).addClass('highlight');
+        }
+      },
+      
+      // mouseout
+      function(){
+        // unhighlight all stars
+        $('ul.any_vote_points_widget li').removeClass('highlight');
+
+        // restore average-active class
+        for(i=0; i<average_count; i++) {
+          $('ul.any_vote_points_widget li').eq(i).find('a').addClass('average-active');
+        }
+      }
+    );
+  });
+});
